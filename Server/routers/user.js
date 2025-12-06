1ba7851b5b7277e57ae46c5ab6be84dab528f654
Rutuja-coder08 <jadhavrutuja28@gmail.com>
2025-12-06 22:08:18 +0530

    Routes

---
+++ Server/routers/user.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const jwt =require('jsonwebtoken')
+const config = require('../utils/config')
+
+const pool = require('../utils/db');
+const result = require('../utils/result');
+
+const router = express.Router();
+const saltRounds = 10;
+
