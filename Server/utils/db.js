84d6753e17040a1d773362b37d2a5360ba9b05ad
MaheshSamale <maheshsamale7@gmail.com>
2025-12-07 09:47:25 +0530

    user login and register

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Rutuja0802@',
-    database: 'blogs'
+    password: 'Mahesh@123',
+    database: 'hirewrite_database'
 })
 
 module.exports = pool
